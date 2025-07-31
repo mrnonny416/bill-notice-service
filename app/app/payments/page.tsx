565542d15ad049adedf076851c3fd267f59f0cdc
mrnonny <68236769+mrnonny416@users.noreply.github.com>
2025-08-01 00:47:21 +0700

    Add payments page UI with form and bank selection
    Introduces a new payments page featuring a notice, input for repayment amount, bank selection dropdown, and a next step button. The UI is styled and includes Thai language instructions for users.

---
+++ app/app/payments/page.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { HiOutlineSpeakerWave } from "react-icons/hi2";
+
+export default function Payments() {
+  return (
+    <>
+      <div className="mx-auto flex w-full items-center justify-center bg-orange-100 text-orange-500">
+        <div className="flex items-center justify-center gap-2 p-3">
+          <HiOutlineSpeakerWave />
+        </div>
+        <div className="p-2">
+          ชำระตรงเวลา ยืมใหม่ปลดล็อกจำนวนเงินที่สูงขึ้น
+          และอันดับเครดิตก็จะดีขึ้นด้วย
+        </div>
+      </div>
+
+      <div className="mx-auto mt-4 max-w-md">
+        <div className="flex w-full flex-col items-center gap-2 p-3">
+          <div className="text-sm text-gray-400">
+            กรุณาใส่จำนวนเงินที่ชำระคืนของคุณ
+            <span className="text-yellow-500">
+              {" "}
+              (จำนวนเงินที่ชำระคืนจะต้องสอดคล้องกับการโอนเงินจริง)
+            </span>
+          </div>
+          <div className="relative w-full">
+            <span className="pointer-events-none absolute top-1/2 left-3 -translate-y-1/2 text-lg text-gray-400">
+              ฿
+            </span>
+            <input
+              type="number"
+              className="w-full rounded-md border-1 border-gray-400 p-3 pl-8"
+              placeholder="จำนวนเงินที่ต้องการชำระคืน"
+            />
+          </div>
+          <div className="mt-2 flex w-full justify-start text-sm text-gray-400">
+            โปรดเลือกบัตรธนาคาร
+          </div>
+          <div className="flex w-full justify-start text-gray-400">
+            <div className="relative w-full">
+              <select className="w-full rounded-md border-1 border-gray-400 p-3 pl-8">
+                <option value="">โปรดเลือกบัตรธนาคาร</option>
+                <option value="bank1">ธนาคาร 1</option>
+                <option value="bank2">ธนาคาร 2</option>
+                <option value="bank3">ธนาคาร 3</option>
+              </select>
+            </div>
+          </div>
+          <div className="mt-5 flex w-full justify-start text-gray-400">
+            <button className="h-fit w-full rounded-4xl bg-indigo-600 py-3 text-white">
+              ขั้นตอนต่อไป
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
